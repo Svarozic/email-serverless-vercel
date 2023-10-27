dd1d5ee625d981168623b49f3ab5addc8f2d01bf
Peter Petraník <peter.petranik@bitmovin.com>
2023-10-27 16:40:04 +0200

    "@sendgrid/mail": "^7.7.0",

--- app/api/serverless-example/route.ts
+++ app/api/serverless-example/route.ts
@@ -1,15 +1,39 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-export function GET(request: NextRequest) {
+
+import { NextRequest, NextResponse } from 'next/server';
+import sendgrid from "@sendgrid/mail";
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY as string);
+
+export async function GET(req: NextRequest, res: NextResponse) {
+    try {
+        console.log("REQ.BODY", req.body);
+        await sendgrid.send({
+            to: "petranikpeter@gmail.com", // Your email where you'll receive emails
+            from: "ivanapeter2024@gmail.com", // your website email address here
+            subject: `Ivana & Peter Wedding 2024`,
+            html: `<div>You've got a mail</div>`,
+        });
+    } catch (error: any) {
+        console.log(error);
+        return NextResponse.json(
+            { error: error.message },
+            {
+                status: error.statusCode || 500,
+            },
+        );
+    }
+
+
     return NextResponse.json(
         {
-            body: request.body,
-            path: request.nextUrl.pathname,
-            query: request.nextUrl.search,
-            cookies: request.cookies.getAll(),
+            body: "Thank you",
+            path: req.nextUrl.pathname,
+            query: req.nextUrl.search,
+            cookies: req.cookies.getAll(),
         },
         {
             status: 200,
         },
     );
+
 }
